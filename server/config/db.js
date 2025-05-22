30bcb9c12b18ab4122b639e523db7345b4421876
Chetan Reddy <322103310203@gvpce.ac.in>
2025-05-22 17:51:46 +0530

    Backend Setup Completed

---
+++ server/config/db.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+
+//Function to connect to the MONGODB database
+
+const connectDB = async() =>{
+    mongoose.connection.on('connected', () => console.log('MongoDb connected'))
+
+    await mongoose.connect(`${process.env.MONGODB_URI}/job-portal`)
+}
+
+export default connectDB
---
+++ server/config/instrument.js
@@ -0,0 +1,11 @@
+// Import with `import * as Sentry from "@sentry/node"` if you are using ESM
+import * as Sentry from "@sentry/node"
+
+Sentry.init({
+  dsn: "https://371c1b9915069744687150eea92544d1@o4509366715613184.ingest.us.sentry.io/4509366719807488",
+  integrations: [Sentry.mongooseIntegration()],
+
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true,
+});
