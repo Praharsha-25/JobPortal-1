9c74d5db4dcbd6f18f32966fffac5b58726dc5f6
HemanthSambangi2901 <hemanthsambangi2901@gmail.com>
2025-05-22 09:01:51 +0530

    Remove node_modules from tracking and add to .gitignore

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -10,6 +10,8 @@ export const AppContextProvider = (props) => {
 
     const [jobs, setJobs] = useState([])
 
+    const [showRecruiterLogin,setShowRecruiterLogin] = useState(false)
+
     // Function to fetch jobs
     const fetchJobs = async () =>{
         setJobs(jobsData)
@@ -21,7 +23,9 @@ export const AppContextProvider = (props) => {
     const value = {
         setSearchFilter, searchFilter,
         isSearched, setIsSearched,
-        jobs, setJobs
+        jobs, setJobs,
+        showRecruiterLogin, setShowRecruiterLogin,
+        
     }
     return (
         <AppContext.Provider value={value}>
