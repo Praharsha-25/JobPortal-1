5f72d2efb327d57678e7de8fd9cda78e8bc3d0d8
HemanthSambangi2901 <hemanthsambangi2901@gmail.com>
2025-05-23 21:18:39 +0530

    Commiting for Backend Deployment

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,30 @@
+// import mongoose from 'mongoose';
+
+// //Function to connect to the MONGODB database
+
+// const connectDB = async() =>{
+//     mongoose.connection.on('connected', () => console.log('MongoDb connected'))
+
+//     await mongoose.connect(`${process.env.MONGODB_URI}/job-portal`)
+// }
+
+// export default connectDB
+
+
+
 import mongoose from 'mongoose';
 
-//Function to connect to the MONGODB database
+const connectDB = async () => {
+  try {
+    const MONGO_URI = "mongodb+srv://haroon:haroongfprasanna@jobportalcluster.tqa3dw3.mongodb.net/jobportal?retryWrites=true&w=majority";
 
-const connectDB = async() =>{
-    mongoose.connection.on('connected', () => console.log('MongoDb connected'))
+    const conn = await mongoose.connect(MONGO_URI);
 
-    await mongoose.connect(`${process.env.MONGODB_URI}/job-portal`)
-}
+    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`❌ Error: ${error.message}`);
+    process.exit(1);
+  }
+};
 
-export default connectDB
+export default connectDB;
--- server/server.js
+++ server/server.js
@@ -4,9 +4,13 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/db.js'
 import * as Sentry from "@sentry/node";
+import { clerkWebhooks  } from './controllers/webhooks.js'
 
 //Initialize express
 const app = express()
+
+console.log('MONGO_URI from .env:', process.env.MONGO_URI);
+
 
 //Database connection
 await connectDB()
@@ -20,6 +24,7 @@ app.get('/', (req, res) => res.send("API is running..."))
 app.get("/debug-sentry", function mainHandler(req, res) {
   throw new Error("My first Sentry error!");
 });
+app.post('/webhooks',clerkWebhooks)
 
 
 //Port
