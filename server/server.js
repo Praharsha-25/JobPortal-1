2f6f2e0281c72bffab8a86d60bf4c3aa80142914
HemanthSambangi2901 <hemanthsambangi2901@gmail.com>
2025-05-27 10:23:57 +0530

    Controller function is completed and we need install package by running the command "npm install @clerk/express"
    next we have to integrate the api

--- server/server.js
+++ server/server.js
@@ -5,17 +5,26 @@ import connectDB from './config/db.js'
 import './config/instrument.js'
 import * as Sentry from "@sentry/node";
 import { clerkWebhooks } from './controllers/webhooks.js'
+import companyRoutes from './routes/companyRoutes.js'
+import connectCloudinary from './config/cloudinary.js'
+import jobRoutes from './routes/jobRoutes.js'
+import userRoutes from './routes/userRoutes.js'
+import {clerkMiddleware} from '@clerk/express'
+
 
 //Intitialize express 
 const app = express()
 
 //connect to database
 await connectDB()
+await connectCloudinary()
+
 
 //Middlewares
 app.use(cors())
 app.use('/webhooks', express.raw({ type: 'application/json' }));
 app.use(express.json()); // keep this after raw
+app.use(clerkMiddleware());
 
 //Routes
 app.get('/', (req, res) => res.send("API Working"))
@@ -23,6 +32,9 @@ app.get("/debug-sentry", function mainHandler(req, res) {
   throw new Error("My first Sentry error!");
 });
 app.post('/webhooks', clerkWebhooks)
+app.use('/api/company',companyRoutes)
+app.use('/api/jobs',jobRoutes)
+app.use('/api/users',userRoutes)
 
 //Port
 const PORT = process.env.PORT || 5000
